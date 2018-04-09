a85fbcdb43cfdab65fd0ce28d24d55f1a78efc99
Vicki Hamm <vicki.cello@gmail.com>
2018-04-09 02:33:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,9 @@ function removeElementFromBeginningOfArray(array) {
   array.slice(1);
   return array;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
