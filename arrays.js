c4b6f9a6a4d924c803dc78bd23c2ca2289b17b87
Vicki Hamm <vicki.cello@gmail.com>
2018-04-09 02:54:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, element];
-  return array;
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
